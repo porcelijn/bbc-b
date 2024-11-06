d7d0669b8db50c85411aac7318c3956d3035f494
Taine <taine>
2024-11-06 17:20:38 +0100

    6502 registers

---
+++ src/mos6502/registers.rs
@@ -0,0 +1,14 @@
+pub struct Registers {
+  pub a: u8,   // accumulator
+  pub x: u8,
+  pub y: u8,
+  pub p: u8,   // status
+  pub pc: u16, // program counter
+  pub s: u8,   // page 0x01 stack pointer offset
+}
+
+impl Registers {
+  pub const fn new() -> Self {
+    Registers { a: 0, x: 0, y: 0, p: 0, pc: 0, s: 0xff }
+  }
+}
