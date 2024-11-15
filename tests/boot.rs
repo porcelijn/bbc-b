38862fd367c90800b6b7aaa20c4edc63be0062f1
porcelijn <taine>
2024-11-15 12:41:26 +0100

    Split MOS reset test  - keep original as-is reset test  - also test with zeroed SHEILA page "hard" reset

--- tests/boot.rs
+++ tests/boot.rs
@@ -15,15 +15,12 @@ fn dump(filename: &str, bytes: &[u8]) {
 
 #[test]
 fn os120_reset() {
-  const CLEAR_SHEILA: bool = true;
-
   let mut ram = RAM::new();
 
   // manually compiled from https://tobylobster.github.io/mos/os120_acme.a
   ram.load_bin_at("images/os120.bin", Address::from(0xC000));
-  if CLEAR_SHEILA {
-    ram.load_at(&[0; 256], Address::from(0xFE00)); // clear SHEILA page
-  }
+  // Use whatever garbage is in the MOS 1.20 image at SHEILA addresses
+
   let mut cpu = CPU::new();
   cpu.reset(&mut ram);
   assert_eq!(cpu.registers.pc, Address::from(0xD9CD)); // .resetEntryPoint
@@ -34,20 +31,42 @@ fn os120_reset() {
     cpu.step(&mut ram);
   }
   {
-    let (a, p, pc) = match CLEAR_SHEILA {
-      // Sheila is 0-initialized RAM
-      true =>  (0,    0b0010_0110, 0xD9E7),
-      // Use whatever garbage is in the MOS 1.20 image at SHEILA addresses
-      false => (210,  0b1010_0100, 0xD9DE),
-    };
-
     let r = &cpu.registers;
-    assert_eq!(r.a, a);
+    assert_eq!(r.a, 210);
     assert_eq!(r.x, 255);
     assert_eq!(r.y, 0);
-    assert_eq!(r.p.to_u8(), p);
+    assert_eq!(r.p.to_u8(), 0b1010_0100);
     assert_eq!(r.s.to_u8(), 254);
-    assert_eq!(r.pc.to_u16(), pc);
+    assert_eq!(r.pc.to_u16(), 0xD9DE); // skip reset memory
+  }
+
+  cpu.run(&mut ram, &stop_after::<1_000_000>);
+}
+
+
+#[test]
+fn os120_clear_sheila() {
+  let mut ram = RAM::new();
+  ram.load_bin_at("images/os120.bin", Address::from(0xC000));
+  ram.load_at(&[0; 256], Address::from(0xFE00)); // clear SHEILA page
+
+  let mut cpu = CPU::new();
+  cpu.reset(&mut ram);
+  assert_eq!(cpu.registers.pc, Address::from(0xD9CD)); // .resetEntryPoint
+  for _ in 0..10 {
+    let slice = slice(&mut ram, cpu.registers.pc, 3);
+    let dump = disassemble_with_address(cpu.registers.pc, &slice);
+    println!("{dump}");
+    cpu.step(&mut ram);
+  }
+  {
+    let r = &cpu.registers;
+    assert_eq!(r.a, 0);
+    assert_eq!(r.x, 255);
+    assert_eq!(r.y, 0);
+    assert_eq!(r.p.to_u8(), 0b0010_0110);
+    assert_eq!(r.s.to_u8(), 254);
+    assert_eq!(r.pc.to_u16(), 0xD9E7); // reset memory
   }
 
   cpu.run(&mut ram, &stop_after::<10_000_000>);
