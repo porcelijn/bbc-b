183141c2924e6fdc47bf075abd8a6a1a3c5ae8dd
taine <most-starving-drum@duck.com>
2024-12-11 10:28:07 +0100

    Cleanup

--- crates/screen/src/lib.rs
+++ crates/screen/src/lib.rs
@@ -5,15 +5,20 @@ const WIDTH: usize = 320;
 const HEIGHT: usize = 256;
 
 const COLUMNS: usize = WIDTH / 8; // 40
-const ROWS: usize = WIDTH / 8; // 32
+const _ROWS: usize = WIDTH / 8; // 32
+
+// 3 bit RGB color
+#[allow(non_camel_case_types)]
+type u3 = u8;
+type Buffer =[u32; WIDTH * HEIGHT]; // 24 bits RGB
 
 pub struct Screen {
-  buffer: [u32; WIDTH * HEIGHT], // 24 bits RGB
+  buffer: Buffer,
   window: Window,
 }
 
 impl Screen {
-  const fn color_from_u3(rgb3: u8) -> u32 {
+  const fn color_from_u3(rgb3: u3) -> u32 {
     assert!(rgb3 < 8);
     let mut rgb24 = 0u32;
     let mut mask = 0b100;
@@ -36,11 +41,22 @@ impl Screen {
   const YELLOW:  u32 = Self::color_from_u3(0b110);
   const WHITE:   u32 = Self::color_from_u3(0b111);
 
+  const COLORS: [u32; 8] = [
+    Self::BLACK,
+    Self::BLUE,
+    Self::GREEN,
+    Self::CYAN,
+    Self::RED,
+    Self::MAGENTA,
+    Self::YELLOW,
+    Self::WHITE,
+  ];
+
   pub fn new(title: &str) -> Self {
     let mut window_options = WindowOptions::default();
     window_options.scale = minifb::Scale::X2;
     let mut window = Window::new(title, WIDTH, HEIGHT, window_options)
-              .unwrap_or_else(|e| { panic!("failed to open Window {}", e); });
+      .unwrap_or_else(|e| { panic!("failed to open Window {}", e); });
 
     // Limit to max ~50 fps update rate
     window.set_target_fps(50);
@@ -56,24 +72,24 @@ impl Screen {
     // We unwrap here as we want this code to exit if it fails. Real
     // applications may want to handle this in a different way
     self.window
-        .update_with_buffer(&self.buffer, WIDTH, HEIGHT)
-        .unwrap();
+      .update_with_buffer(&self.buffer, WIDTH, HEIGHT)
+      .unwrap();
   }
 
   pub fn blit(&mut self, video_ram: &[u8]) {
     for y in 0..HEIGHT {
       let mut target = y * WIDTH;
       let mut source = (y % 8) + (y / 8) * WIDTH;
-      for x in 0..(WIDTH / 8) {
+      for x in 0..(COLUMNS) {
         assert!(y < HEIGHT);
-        assert!(x < COLUMNS * 8);
         assert!(x * 8 < WIDTH);
         assert!((x * 8 + (y % 8) + (y / 8) * WIDTH) < WIDTH * HEIGHT); 
         assert_eq!(source, x * 8 + (y % 8) + (y / 8) * WIDTH); 
+        assert_eq!(target, x * 8 + y * WIDTH);
         let mut byte = video_ram[source];
         for _ in 0..8 {
           let color = if 0b1000_0000 & byte == 0 {
-            Self::BLUE >> 2
+            Self::BLUE >> 2 // looks better than black
           } else {
             Self::WHITE
           };
@@ -88,26 +104,9 @@ impl Screen {
     }
   }
 
-  pub fn update(&mut self, time: u32) {
-    // bgr
-    let rgb = [0x0000ff, 0x00ff00, 0xff0000,
-        0xFF0000ff, 0xFF00ff00, 0xFFff0000];
-    let mut a = time;
-    for i in self.buffer.iter_mut() {
-      *i = rgb[(a as usize/10)%6]; // write something more funny here!
-      a+=1;
-    }
-  }
-
-  pub fn draw_line(&mut self) {
-    for x in 0..WIDTH {
-      let y = (HEIGHT-1) - HEIGHT * x / WIDTH;
-      self.buffer[x + WIDTH * y] = 0xFFFFFF;
-    }
-  }
-
-  pub fn pixel(&mut self, x: usize, y: usize) {
-    self.buffer[x + WIDTH * y] = Self::WHITE;
+  pub fn pixel(&mut self, x: usize, y: usize, c: u3) {
+    assert!(c < 8);
+    self.buffer[x + WIDTH * y] = Self::COLORS[c as usize];
   }
 }
 
@@ -115,13 +114,29 @@ impl Screen {
 mod tests {
   use super::*;
 
+  fn update(buffer: &mut Buffer, time: u32) {
+    let mut seed = time as usize;
+    for iter in buffer.iter_mut() {
+      *iter = Screen::COLORS[(seed / 10) % 6]; // write something more funny here!
+      seed += 1;
+    }
+  }
+
+  fn draw_line(buffer: &mut Buffer) {
+    for x in 0..WIDTH {
+      let y = (HEIGHT - 1) - HEIGHT * x / WIDTH;
+      buffer[x + WIDTH * y] = 0xFFFFFF;
+    }
+  }
+
+
 #[test]
   fn it_works() {
     let mut buffer = Screen::new("Test (ESC to exit, time out after 2s)");
     let mut time = 0;
     while !buffer.done() && time < 2 * 50 {
-      buffer.update(time);
-      buffer.draw_line();
+      update(&mut buffer.buffer, time);
+      draw_line(&mut buffer.buffer);
       buffer.show();
       time += 1;
     }
