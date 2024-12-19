806ab603a4dd780c0eca14d624d0eb7762881bc9
taine <most-starving-drum@duck.com>
2024-12-19 14:18:16 +0100

    Remove keyboard buffer hacking With the `CMP` overflow flag fix we now have a ''working'' MOS keyboard It still somewhat stutters, though

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use bbc_b::mos6502::{CPU, stop_at};
 use bbc_b::devices::{ClockedDevices, DevicePage, SheilaPage};
 use bbc_b::devices::keyboard::Keyboard;
 use bbc_b::host::Screen;
-use bbc_b::memory::{Address, MemoryBus, PageDispatcher, read_address};
+use bbc_b::memory::{Address, PageDispatcher, read_address};
 use bbc_b::memory::ram::RAM;
 
 fn vdu_to_terminal(a_register: u8) {
@@ -22,112 +22,6 @@ fn vdu_to_terminal(a_register: u8) {
     // for now, ignore multitude of control codes
     _ => {},
   }
-}
-
-#[derive(PartialEq)]
-struct BufIter(u8); // X register
-
-impl BufIter {
-  // Keyboard buffer 3E0-3FF (32 bytes)
-  const BASE_ADDRESS:  Address = Address::from(0x0300);
-  const START_OFFSET:       u8 = 0xe0;
-  const CAPACITY:           u8 = 32;
-  const EMPTY_FLAG:    Address = Address::from(0x02cf);
-  const START_POINTER: Address = Address::from(0x02d8);
-  const END_POINTER:   Address = Address::from(0x02e1);
-
-  fn start(memory: &dyn MemoryBus) -> Self {
-    Self(memory.read(Self::START_POINTER))
-  }
-
-  fn end(memory: &dyn MemoryBus) -> Self {
-    Self(memory.read(Self::END_POINTER))
-  }
-
-  fn empty(memory: &dyn MemoryBus) -> bool {
-//  memory.read(Self::EMPTY_FLAG) & 0b1000_0000 == 0
-    Self::size(memory) == 0
-  }
-
-  fn size(memory: &dyn MemoryBus) -> u8 {
-    Self::distance(Self::start(memory), Self::end(memory))
-  }
-
-  const fn distance(start: Self, end: Self) -> u8 {
-    if start.0 <= end.0 {
-      end.0 - start.0
-    } else {
-      Self::CAPACITY - (start.0 - end.0)
-    }
-  }
-
-  fn address(&self) -> Address {
-    let mut address = Self::BASE_ADDRESS;
-    address.inc_by(self.0);
-    address
-  }
-
-  fn next(&mut self) {
-    if self.0 == 0xFF {
-      self.0 = Self::START_OFFSET;
-    } else {
-      self.0 += 1;
-    }
-  }
-
-  fn save_start(&self, memory: &mut dyn MemoryBus) {
-    memory.write(Self::START_POINTER, self.0);
-  }
-
-  fn save_end(&self, memory: &mut dyn MemoryBus) {
-    memory.write(Self::END_POINTER, self.0);
-  }
-}
-
-fn dump_keyboard_buffer(memory: &dyn MemoryBus) {
-  let mut first = BufIter::start(memory);
-  let last = BufIter::end(memory);
-  let empty = BufIter::empty(memory);
-  let size = BufIter::size(memory);
-  print!("keyboard buffer, empty={empty}, size={size}:");
-  while first != last {
-    let value = memory.read(first.address());
-    print!(" '{}' ({value}),", value as char);
-    first.next();
-  }
-  println!();
-}
-
-fn insert_keyboard_buffer(memory: &mut dyn MemoryBus, value: u8) {
-  if value == 0x1b { // ASCII 27 -> escape
-    const ESCAPE_FLAG: Address = Address::from(0x00ff);
-    const ASCII_ESCAPE_ACTION: Address = Address::from(0x026C);
-    assert_eq!(value, memory.read(ASCII_ESCAPE_ACTION));
-    println!("ESCAPEFLAG was: {}", memory.read(ESCAPE_FLAG));
-    memory.write(ESCAPE_FLAG, 0x80);
-    return;
-  }
-  let first = BufIter::start(memory);
-  let mut last = BufIter::end(memory);
-  memory.write(last.address(), value);
-  last.next();
-  assert!(last != first);
-  last.save_end(memory);
-  memory.write(BufIter::EMPTY_FLAG, 0x00);
-}
-
-fn remove_keyboard_buffer(memory: &mut dyn MemoryBus, pop_value: bool) -> u8 {
-  assert!(!BufIter::empty(memory));
-  assert!(BufIter::size(memory) > 0);
-  let mut first = BufIter::start(memory);
-  let last = BufIter::end(memory);
-  let value = memory.read(first.address());
-  if pop_value {
-    first.next();
-    first.save_start(memory);
-    memory.write(BufIter::EMPTY_FLAG, 0xFF);
-  }
-  value
 }
 
 fn main() {
@@ -178,30 +72,19 @@ fn main() {
     if cpu.registers.p.has::<'I'>() {
       continue;
     }
+
     if wait_a_while == 0 {
       wait_a_while = 100;
       let new_key = screen.borrow().try_read();
       if new_key != last_key {
-        dump_keyboard_buffer(&*mem.borrow());
-        // pop magic key if present
-        const F10: u8 = 0xCA;
-        if !BufIter::empty(&mut *mem.borrow_mut()) &&
-           remove_keyboard_buffer(&mut *mem.borrow_mut(), false) == F10 {
-          assert_eq!(remove_keyboard_buffer(&mut *mem.borrow_mut(), true), F10);
-        }
         if let Some(key) = last_key {
-          println!("Release '{}' ({key})", key as char);
-         // keyboard.borrow_mut().release_key_ascii(key);
+          keyboard.borrow_mut().release_key_ascii(key);
         }
 
         if let Some(key) = new_key {
-          println!("Press '{}' ({key})", key as char);
-       //   keyboard.borrow_mut().press_key_ascii(key);
-          insert_keyboard_buffer(&mut *mem.borrow_mut(), key);
-          wait_a_while = 1000;
+          keyboard.borrow_mut().press_key_ascii(key);
+          wait_a_while = 500;
         }
-
-        dump_keyboard_buffer(&*mem.borrow());
       }
       last_key = new_key;
     } else {
